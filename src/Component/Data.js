5574399ff41320f3835c4bfffc0f593426616d06
Sugitha08 <142788570+Sugitha08@users.noreply.github.com>
2024-10-11 16:41:55 +0530

    overall Design

---
+++ src/Component/Data.js
@@ -0,0 +1,99 @@
+import TechLogo from "../Assets/company-avatar-65bce8a217ef2-x2.png"
+import OneSolution from "../Assets/globe-data-tech-solution-600w-693989875.webp"
+
+export const data = [
+  {
+    id: 1,
+    logo: TechLogo,
+    jobTitle: "Software Engineer",
+    companyName: "Tech Innovators",
+    experienceRequired: 3,
+    skillsRequired: ["JavaScript", "React", "Node.js", "CSS"],
+    jobDescription:
+      "We are looking for a skilled software engineer with experience in building web applications using React and Node.js.",
+  },
+  {
+    id: 2,
+    logo: OneSolution,
+    jobTitle: "Data Scientist",
+    companyName: "Data Solutions",
+    experienceRequired: 2,
+    skillsRequired: ["Python", "Machine Learning", "SQL", "R"],
+    jobDescription:
+      "Join our team as a data scientist to help us analyze large data sets and derive actionable insights for business decisions.",
+  },
+  {
+    id: 3,
+    logo: "https://companylogos.com/logo3.png",
+    jobTitle: "UX Designer",
+    companyName: "Creative Designs Inc.",
+    experienceRequired: 4,
+    skillsRequired: ["Figma", "Adobe XD", "Prototyping", "UI/UX Design"],
+    jobDescription:
+      "We are seeking a creative UX designer with a passion for user-centric design and experience in creating wireframes and prototypes.",
+  },
+  {
+    id: 4,
+    logo: "https://companylogos.com/logo4.png",
+    jobTitle: "Full Stack Developer",
+    companyName: "Web Masters",
+    experienceRequired: 5,
+    skillsRequired: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node.js",
+    ],
+    jobDescription:
+      "Looking for a full stack developer with extensive experience in both front-end and back-end technologies. You will be working on creating dynamic and scalable web applications.",
+  },
+  {
+    id: 5,
+    logo: "https://companylogos.com/logo5.png",
+    jobTitle: "Project Manager",
+    companyName: "Project Leaders Co.",
+    experienceRequired: 6,
+    skillsRequired: ["Agile", "Scrum", "Project Management", "Team Leadership"],
+    jobDescription:
+      "Lead our project teams to success by ensuring timely delivery of products and overseeing the project lifecycle from inception to completion.",
+  },
+];
+
+
+
+
+export const skillOptions = [
+  { value: "html", label: "HTML" },
+  { value: "css", label: "CSS" },
+  { value: "javascript", label: "JavaScript" },
+  { value: "react", label: "React" },
+  { value: "redux", label: "Redux" },
+  { value: "material-ui", label: "Material UI" },
+  { value: "tailwind-css", label: "Tailwind CSS" },
+  { value: "bootstrap", label: "Bootstrap" },
+  { value: "nodejs", label: "Node.js" },
+  { value: "expressjs", label: "Express.js" },
+  { value: "mongodb", label: "MongoDB" },
+  { value: "firebase", label: "Firebase" },
+  { value: "graphql", label: "GraphQL" },
+  { value: "docker", label: "Docker" },
+  { value: "git", label: "Git" },
+  { value: "postman", label: "Postman" },
+  { value: "swagger", label: "Swagger" },
+  { value: "typescript", label: "TypeScript" },
+  { value: "python", label: "Python" },
+  { value: "django", label: "Django" },
+  { value: "flask", label: "Flask" },
+  { value: "mysql", label: "MySQL" },
+  { value: "postgresql", label: "PostgreSQL" },
+  { value: "java", label: "Java" },
+  { value: "spring-boot", label: "Spring Boot" },
+  { value: "csharp", label: "C#" },
+  { value: "dotnet", label: ".NET" },
+  { value: "php", label: "PHP" },
+  { value: "laravel", label: "Laravel" },
+  { value: "angular", label: "Angular" },
+];
---
+++ src/Component/JobModal.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import { Tag } from "primereact/tag";
+import { Button } from "primereact/button";
+
+function JobModal({ visible, setVisible, data }) {
+  console.log(data);
+
+  const handleClose = () => {
+    setVisible(false);
+  };
+  const handleFormOpen = () => {};
+  return (
+    <Modal show={visible} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <img
+          src={data?.logo}
+          alt="logo"
+          style={{ fontSize: "10px", borderRadius: "50%", width: "45px" }}
+        />
+
+        <div className="ms-2">
+          <h4 className="mb-0 ">
+            <img src="" />
+            {data?.companyName}
+          </h4>
+          <div>{data?.jobTitle}</div>
+        </div>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="jobDis">
+          <h5 className="mb-0">About the job</h5>
+          <div>
+            <h6 className="mb-0">Experience Required :  {data?.experienceRequired}years</h6>
+          </div>
+          <div>
+            <h6 className="mb-2">Skills Required : </h6>
+            <div className="">
+              {data?.skillsRequired?.map((skill) => (
+                <Tag
+                  style={{ background: "#f4f4f4", color: "#545454" }}
+                  className="px-2 py-1 mx-1 my-1"
+                  value={skill}
+                ></Tag>
+              ))}
+            </div>
+          </div>
+          <div>
+            <h6>Job Description :  </h6>
+            <p style={{ textIndent: "24px" }}>{data?.jobDescription}</p>
+          </div>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          label="Apply for Job"
+          className="float-end p-2"
+          onClick={handleFormOpen}
+          style={{ borderRadius: "7px",fontFamily:"inter var" }}
+        />
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default JobModal;
